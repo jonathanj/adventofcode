d0a9309aa3f6830653d3327ccfd6a2a1fc7117ce
Jonathan Jacobs <jonathan@jsphere.com>
2016-12-04 16:57:44 +0200

    Implement part 2 of the Clojure day 3 solution.

--- clojure/src/advent2016/day3.clj
+++ clojure/src/advent2016/day3.clj
@@ -2,18 +2,25 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as s]))
 
+(def puzzle (line-seq (io/reader "../day3.data")))
+
+
 (defn read-sides [line]
   (map #(Integer/parseInt %) (remove s/blank? (s/split line #" "))))
-
 
 (defn valid-triangle? [[a b c]]
   (and (> (+ a b) c)
        (> (+ a c) b)
        (> (+ b c) a)))
 
-(def solution (->> "../day3.data"
-                 io/reader
-                 line-seq
-                 (map read-sides)
-                 (filter valid-triangle?)
-                 count))
+(def solution1 (->> puzzle
+                    (map read-sides)
+                    (filter valid-triangle?)
+                    count))
+
+(def solution2 (->> puzzle
+                    (map read-sides)
+                    (apply mapcat vector)
+                    (partition 3)
+                    (filter valid-triangle?)
+                    count))
