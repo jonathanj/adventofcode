dff5a8650c5d7ed5b5bab12b36ac5d61541dbb4e
Jonathan Jacobs <jonathan@jsphere.com>
2016-12-04 16:49:20 +0200

    Implement part 2 of day 3 Python solution.

--- python/day3.py
+++ python/day3.py
@@ -1,4 +1,14 @@
 import sys
+import itertools
+
+
+def grouper(iterable, n, fillvalue=None):
+    args = [iter(iterable)] * n
+    return itertools.izip_longest(fillvalue=fillvalue, *args)
+
+
+def transpose(xs):
+    return zip(*xs)
 
 
 def read_sides(line):
@@ -10,4 +20,7 @@ def valid_triangle((a, b, c)):
 
 
 if __name__ == '__main__':
-    print len(filter(valid_triangle, map(read_sides, sys.stdin)))
+    print len(filter(valid_triangle,
+                     grouper(
+                         itertools.chain.from_iterable(
+                             transpose(map(read_sides, sys.stdin))), 3)))
