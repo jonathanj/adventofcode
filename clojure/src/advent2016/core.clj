f6e551601f7ec61eaf8d1ac5c0e03696b8964364
Jonathan Jacobs <jonathan@jsphere.com>
2016-12-14 12:17:16 +0200

    My own (probably naive) lazy breadth-first search implementation.

--- clojure/src/advent2016/core.clj
+++ clojure/src/advent2016/core.clj
@@ -1,5 +1,6 @@
 (ns advent2016.core
   (:import [java.security MessageDigest])
+  (:require [flatland.useful.seq :refer [lazy-loop]])
   (:gen-class))
 
 (set! *warn-on-reflection* true)
@@ -8,6 +9,21 @@
 
 (defn ^String md5 [^String s]
   (format "%032x" (BigInteger. 1 (.digest (doto md5-digest .reset) (.getBytes s)))))
+
+(defn bfs [adjacent start]
+  (lazy-loop [seen #{}
+              xs   [start]
+              ys   []]
+    (let [x (first xs)]
+      (cond
+        (and (empty? xs)
+             (empty? ys)) (list)
+        (empty? xs)       (lazy-recur seen (reverse ys) [])
+        (seen x)          (lazy-recur seen (rest xs) ys)
+        :else             (cons x (lazy-recur (conj seen x)
+                                              (rest xs)
+                                              (concat (adjacent x) ys)))))))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
