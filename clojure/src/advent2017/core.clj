ea5962d60799e4832cc74ccae87d83f46eb7bbe7
Jonathan Jacobs <jonathan@jsphere.com>
2017-12-03 17:57:58 +0200

    Day 3 2017 solution.

--- clojure/src/advent2017/core.clj
+++ clojure/src/advent2017/core.clj
@@ -6,3 +6,10 @@
 (defn lines->numbers [lines]
   (for [line lines]
     (map #(Integer/parseInt %) (clojure.string/split line #"\s+"))))
+
+(defn manhattan-distance
+  ([[x2 y2]]
+   (manhattan-distance [0 0] [x2 y2]))
+  ([[x1 y1] [x2 y2]]
+   (+ (Math/abs (- x1 x2))
+      (Math/abs (- y1 y2)))))
---
+++ clojure/src/advent2017/day3.clj
@@ -0,0 +1,68 @@
+(ns advent2017.day3
+  (:require [advent2017.core :refer [manhattan-distance]]))
+
+
+(def spiral (mapcat (fn [n [a b]] [[n a] [n b]])
+                    (drop 1 (range))
+                    (cycle [[:R :U] [:L :D]])))
+
+
+(defn neighbours [[x y]]
+  [[(dec x) (dec y)]
+   [x (dec y)]
+   [(inc x) (dec y)]
+   [(inc x) y]
+   [(inc x) (inc y)]
+   [x (inc y)]
+   [(dec x) (inc y)]
+   [(dec x) y]])
+
+
+(defn one-instruction [[route [x y]] [n dir] pred next-step]
+  (let [next-pos (case dir
+                   :R [(inc x) y]
+                   :U [x (dec y)]
+                   :L [(dec x) y]
+                   :D [x (inc y)])
+        step     (next-step [route [x y] next-pos])]
+    (if (or (not (pred [route [x y]]))
+            (zero? n))
+      [route [x y]]
+      (recur [(assoc route next-pos step) next-pos]
+             [(dec n) dir]
+             pred
+             next-step))))
+
+
+(defn solve [pred next-step]
+  (loop [result        [{[0 0] 1} [0 0]]
+         [inst & path] spiral]
+    (if-not (pred result)
+      result
+      (recur (one-instruction result inst pred next-step) path))))
+
+
+(defn solve-1 [n]
+  (let [next-step (fn [[route pos _]] (inc (route pos)))
+        pred      (fn [[route pos]] (< (route pos) n))]
+    (manhattan-distance (last (solve pred next-step)))))
+
+
+(defn solve-2 [n]
+  (let [next-step (fn [[route _ pos]]
+                    (apply + (remove nil? (map route (neighbours pos)))))
+        pred      (fn [[route pos]] (<= (route pos) n))
+        [route pos] (solve pred next-step)]
+    (route pos)))
+
+
+(comment
+  (assert (= (solve-1 1) 0))
+  (assert (= (solve-1 12) 3))
+  (assert (= (solve-1 1024) 31))
+  (solve-1 361527)
+
+  (assert (= (solve-2 1) 2))
+  (assert (= (solve-2 4) 5))
+  (assert (= (solve-2 26) 54))
+  (solve-2 361527))
