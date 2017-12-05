3cd1100257fe347e24981b9a24105a5483849d96
Jonathan Jacobs <jonathan@jsphere.com>
2017-12-05 09:56:08 +0200

    Day 5 solution.

---
+++ clojure/src/advent2017/day5.clj
@@ -0,0 +1,30 @@
+(ns advent2017.day5
+  "Mutate the input repeatedly after each jump, with only the mutation differing
+  between part 1 and part 2.")
+
+(def puzzle (->> (clojure.java.io/reader "../2017/day5.data")
+                 (line-seq)
+                 (map #(Integer/parseInt %))
+                 (vec)))
+
+(defn solve [inc-fn input]
+  (loop [input' (transient input)
+         pos    0
+         jumps  1]
+    (let [incr    (nth input' pos)
+          new-pos (+ pos incr)]
+      (if (>= new-pos (count input))
+        jumps
+        (recur (assoc! input' pos (inc-fn incr))
+               new-pos
+               (inc jumps))))))
+
+(def solve-1 (partial solve inc))
+(def solve-2 (partial solve (fn [n] (if (>= n 3) (dec n) (inc n)))))
+
+(comment
+  (assert (= (solve-1 [0 3 0 1 -3]) 5))
+  (solve-1 puzzle)
+
+  (assert (= (solve-2 [0 3 0 1 -3]) 10))
+  (solve-2 puzzle))
