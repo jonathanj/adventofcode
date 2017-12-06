0774f282bdf75dee4dfcebee9d84c245fd0150ed
Jonathan Jacobs <jonathan@jsphere.com>
2017-12-06 09:48:15 +0200

    Day 6 2017 solution.

---
+++ clojure/src/advent2017/day6.clj
@@ -0,0 +1,44 @@
+(ns advent2017.day6
+  "Recursively redistribute the largest memory banks until we reach a state
+  we've seen before. Part 1 ends here, the result is the number of cycles
+  required to reach a previously seen state from the initial state. Part 2 feeds
+  the previously seen state into part 1 to determine the size of the cycle
+  loop.")
+
+(def puzzle (-> (slurp (clojure.java.io/file "../2017/day6.data"))
+                (clojure.string/split #"\s+")
+                (->> (mapv #(Integer/parseInt %)))))
+
+(defn redistribute [memory [idx m]]
+  (loop [memory (assoc memory idx 0)
+         idx    (mod (inc idx) (count memory))
+         m      m]
+    (if (zero? m)
+      memory
+      (recur (update memory idx inc)
+             (mod (inc idx) (count memory))
+             (dec m)))))
+
+(defn max-bank [memory]
+  (let [m (apply max memory)]
+    [(.indexOf memory m) m]))
+
+(defn solve [input]
+  (loop [memory input
+         seen   #{}
+         count  0]
+    (if (seen memory)
+      [count memory]
+      (recur (redistribute memory (max-bank memory))
+             (conj seen memory)
+             (inc count)))))
+
+(def solve-1 (comp first solve))
+(def solve-2 (comp solve-1 second solve))
+
+(comment
+  (assert (= (solve-1 [0 2 7 0]) 5))
+  (solve-1 puzzle)
+
+  (assert (= (solve-2 [0 2 7 0]) 4))
+  (solve-2 puzzle))
