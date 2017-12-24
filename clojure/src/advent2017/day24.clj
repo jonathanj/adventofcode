bc3820f03f7ecdc44fd9459812e4a5af78458d53
Jonathan Jacobs <jonathan@jsphere.com>
2017-12-24 23:27:25 +0200

    Clean up day 24 solution.

--- clojure/src/advent2017/day24.clj
+++ clojure/src/advent2017/day24.clj
@@ -1,40 +1,31 @@
 (ns advent2017.day24
-  ""
-  (:require [loom.alg-generic :as ag :refer [bf-traverse bf-path]]
+  "By definining a successors function that returns the cumulative strength and
+  length of a path we can solve part 1 by finding the maximimum strength value
+  and part 2 by finding the strength of the maximum length value."
+  (:require [loom.alg-generic :refer [bf-span]]
             [advent2017.core :refer [read-puzzle ->lines ->int]]))
 
 (defn lines->puzzle [lines]
   (set (map (fn [s] (mapv ->int (clojure.string/split s #"/"))) lines)))
 
-(def sample-puzzle (lines->puzzle ["0/2"
-                                   "2/2"
-                                   "2/3"
-                                   "3/4"
-                                   "3/5"
-                                   "0/1"
-                                   "10/1"
-                                   "9/10"]))
+(def sample-puzzle (lines->puzzle ["0/2" "2/2" "2/3" "3/4" "3/5" "0/1" "10/1" "9/10"]))
 (def puzzle (read-puzzle "day24.data" (comp lines->puzzle ->lines)))
 
-(defn neighbours [[^long m _ available]]
+(defn neighbours [[^long t ^long c ^long m available]]
   (for [[^long a ^long b :as p] available
-        :let                    [a? (== m a)]
+        :let                    [a? (== m a)
+                                 n  (if a? b a)]
         :when                   (or a? (== m b))]
-    [(if a? b a) p (disj available p)]))
+    [(+ t m n) (inc c) n (disj available p)]))
 
 (defn solve [input]
-  (let [start [0 [0 0] input]
-        paths (-> (ag/bf-span neighbours start)
-                  (ag/trace-paths start))]
-    (map #(mapcat second %) paths)))
+  (apply concat (vals (bf-span neighbours [0 0 0 input]))))
 
 (defn solve-1 [input]
-  (apply max (map #(apply + %) (solve input))))
+  (apply max (map first (solve input))))
 
 (defn solve-2 [input]
-  (let [paths (solve input)
-        m     (count (apply max-key count paths))]
-    (apply max (map #(apply + %) (filter #(= (count %) m) paths)))))
+  (first (apply max-key second (solve input))))
 
 (comment
   (assert (= (solve-1 sample-puzzle) 31))
