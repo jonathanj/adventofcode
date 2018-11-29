e2867ed70d3437d1dc7ed1126ddeb3182e53d1bb
Jonathan Jacobs <jonathan@jsphere.com>
2018-11-29 20:37:06 +0200

    Turn on reflection warnings globally.

--- clojure/project.clj
+++ clojure/project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.3.465"]
                  [digest "1.4.5"]
@@ -13,6 +13,7 @@
                  [org.clojure/data.finger-tree "0.0.2"]
                  [instaparse "1.4.8"]
                  ]
+  :global-vars {*warn-on-reflection* true}
   :main ^:skip-aot advent2016.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
