ab1db46ff691b990c34b6cc2b7eef6b050317f43
Jonathan Jacobs <jonathan@jsphere.com>
2021-12-07 07:55:12 +0200

    2021: Day 4 (Scala3): Refactor into a neater recursive solution.

--- scala3/aoc2021/src/main/scala/Day4.scala
+++ scala3/aoc2021/src/main/scala/Day4.scala
@@ -2,45 +2,37 @@ import aoc2021.Util
 
 object Day4:
   type Board = List[List[Int]]
-  type Input = (Seq[Int], Set[Board])
+  type Input = (List[Int], Seq[Board], Set[Int])
 
   def parse(input: String): Input =
     Util.stringLines(input).filter(_.nonEmpty) match {
       case draw :: rest =>
-        val numbers = draw.split(',').map(_.toInt).toSeq
+        val numbers = draw.split(',').map(_.toInt).toList
         val boards = rest
           // Split rows into numbers
           .map(_.trim.split("\\s+").map(_.toInt).toList)
           // Group rows into boards
-          .grouped(5).toSet
-        (numbers, boards)
+          .grouped(5).toSeq
+        (numbers, boards, Set[Int]())
     }
+
+  // Mark called numbers as -1.
+  def mark(number: Int)(board: Board) = board.map(_.map { x => if x == number then -1 else x })
+  def score(number: Int)(board: Board) = number * board.flatten.filter(_ != -1).sum
+  def isWinner(board: Board) = board.exists(_.forall(_ == -1)) || board.transpose.exists(_.forall(_ == -1))
 
   /**
    * Find the winning scores for every board, in the order they are won.
    */
-  def winningScores(input: Input) =
-    val (allNumbers, allBoards) = input
-    // Generate regular and transposed versions of every board, this is easier to check for winners.
-    val boards = allBoards.map { board => board ++ board.transpose }
-    def winners(numbers: Seq[Int]) = boards.filter(_.exists(_.toSet.subsetOf(numbers.toSet)))
-
-    def score(board: Board, marked: Seq[Int]) =
-      board.flatten.toSet.diff(marked.toSet).sum * marked.last
-
-    allNumbers
-      // Build the numbers for every round, e.g. [[7], [7,4], [7,4,9], …]
-      .scanLeft(Seq[Int]())(_ :+ _)
-      // Find the scores for every winner for every round.
-      .scanLeft((Set[Int](), Set[Board]())) { case ((_, previousWinners), numbers) =>
-        val newWinners = winners(numbers)
-        (newWinners.diff(previousWinners).map(score(_, numbers)), newWinners)
+  def play(numbers: List[Int], boards: Seq[Board], marked: Set[Int] = Set()): Seq[Int] =
+    numbers match {
+      case Seq() => Seq()
+      case number :: numbers => {
+        val (winners, losers) = boards.map(mark(number)).partition(isWinner)
+        winners.map(score(number)) ++ play(numbers, losers, marked + number)
       }
-      // Throw away results with no winners.
-      .filter(!_._1.isEmpty)
-      // Assume that the answers we care about only have a single result.
-      .map(_._1.head)
+    }
 
-  def part1 = winningScores andThen (_.head)
+  def part1 = play.tupled andThen (_.head)
 
-  def part2 = winningScores andThen (_.last)
+  def part2 = play.tupled andThen (_.last)
